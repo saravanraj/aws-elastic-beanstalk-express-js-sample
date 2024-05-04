6a7c414cc00fef630db183fbd4b0d5e7bcfb8352
Saravanraj K <kmmsarav@amazon.com>
2024-05-04 13:44:03 +0530

    Testing by changing line

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first project!'));
+app.get('/', (req, res) => res.send('Testing my application is working or not'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
